1c18c6e293bbe2d117f165269b1f2dc333153f82
utkarsh575 <utkarshlohani575@gmail.com>
2022-04-11 23:52:19 +0530

    :sparkles: added square.c

---
+++ square.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// write a program to to print the square of a number :)
+// algorithm is a sequence or set of instructions in which a task is performed
+int main()
+
+{
+    // start the program
+
+    int x, square; // declaring a variable
+
+
+    printf("enter the value of x:- ");
+    scanf("%d", &x); // take the value from user
+      //infix expression 
+    square = x*x;
+
+    // calculate the square
+
+    printf("the value of x square is :-   %d", square); // print the value of square
+
+    return 0; // end the program
+}
